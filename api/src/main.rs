3ea5b4c63636f081a537f9b988db0e0c95e6cbe1
Jeremy Hanmer <jeremy@dreamhost.com>
2023-04-15 18:06:01 -0700

    wip

--- api/src/main.rs
+++ api/src/main.rs
@@ -3,6 +3,7 @@ use dirk_core::dirk_api::*;
 
 use dirk_core::models::dirk::DirkState;
 use std::net::SocketAddr;
+use std::path::PathBuf;
 
 use std::sync::Arc;
 use walkdir::WalkDir;
@@ -14,7 +15,7 @@ struct Args {
     #[clap(short, long, value_parser, default_value_t = SocketAddr::from(([127, 0, 0, 1], 3000)))]
     listen: SocketAddr,
     #[clap(short, long, value_parser)]
-    ruledir: String,
+    ruledir: PathBuf,
 }
 
 #[tokio::main()]
@@ -22,22 +23,27 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
     let db = get_db().await.expect("Unable to get a Database connection");
     let mut yc = Compiler::new().unwrap();
-    for entry in WalkDir::new(args.ruledir)
-        .into_iter()
-        .filter_map(|e| e.ok())
-    {
-        let tc = Compiler::new().unwrap();
-        let add_result = tc.add_rules_file(entry.path());
-        match add_result {
-            Ok(_) => {
-                println!("Successfully added rule(s) from {}", entry.path().to_string_lossy());
-                yc = yc.add_rules_file(entry.path()).unwrap();
-            },
-            Err(e) => {
-                println!("Failed to add rules from {}: {e}", entry.path().to_string_lossy());
+    if args.ruledir.is_dir() {
+        for entry in WalkDir::new(args.ruledir)
+            .into_iter()
+            .filter_map(|e| e.ok())
+        {
+            let tc = Compiler::new().unwrap();
+            let add_result = tc.add_rules_file(entry.path());
+            match add_result {
+                Ok(_) => {
+                    println!("Successfully added rule(s) from {}", entry.path().to_string_lossy());
+                    yc = yc.add_rules_file(entry.path()).unwrap();
+                },
+                Err(e) => {
+                    println!("Failed to add rules from {}: {e}", entry.path().to_string_lossy());
+                }
             }
         }
+    } else {
+        yc.add_rules_file(args.ruledir);
     }
+
     let rules = yc.compile_rules()?;
     let app_state = Arc::new(DirkState { rules, db });
 
