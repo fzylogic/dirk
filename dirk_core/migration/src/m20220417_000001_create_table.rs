37ac9048315b14b388627d765429cb05ed95300f
Jeremy Hanmer <jeremy@dreamhost.com>
2023-04-17 14:46:13 -0700

    fmt

--- dirk_core/migration/src/m20220417_000001_create_table.rs
+++ dirk_core/migration/src/m20220417_000001_create_table.rs
@@ -1,7 +1,7 @@
-use sea_orm_migration::prelude::*;
-use dirk_core::entities::files;
-use dirk_core::entities::file_rule_match;
 use crate::sea_orm::EntityName;
+use dirk_core::entities::file_rule_match;
+use dirk_core::entities::files;
+use sea_orm_migration::prelude::*;
 
 #[derive(DeriveMigrationName)]
 pub struct Migration;
@@ -24,10 +24,25 @@ impl MigrationTrait for Migration {
                             .primary_key(),
                     )
                     .col(ColumnDef::new(files::Column::Sha1sum).string().not_null())
-                    .col(ColumnDef::new(files::Column::FirstSeen).date_time().not_null())
-                    .col(ColumnDef::new(files::Column::LastSeen).date_time().not_null())
-                    .col(ColumnDef::new(files::Column::LastUpdated).date_time().not_null())
-                    .col(ColumnDef::new(files::Column::FileStatus).enumeration(files::Column::FileStatus, files::Column::FileStatus.into_iter()))
+                    .col(
+                        ColumnDef::new(files::Column::FirstSeen)
+                            .date_time()
+                            .not_null(),
+                    )
+                    .col(
+                        ColumnDef::new(files::Column::LastSeen)
+                            .date_time()
+                            .not_null(),
+                    )
+                    .col(
+                        ColumnDef::new(files::Column::LastUpdated)
+                            .date_time()
+                            .not_null(),
+                    )
+                    .col(ColumnDef::new(files::Column::FileStatus).enumeration(
+                        files::Column::FileStatus,
+                        files::Column::FileStatus.into_iter(),
+                    ))
                     .to_owned(),
             )
             .await?;
@@ -39,32 +54,44 @@ impl MigrationTrait for Migration {
                     .col(
                         ColumnDef::new(file_rule_match::Column::FileId)
                             .integer()
-                            .not_null()
+                            .not_null(),
                     )
-                    .col(ColumnDef::new(file_rule_match::Column::RuleName).string().not_null())
+                    .col(
+                        ColumnDef::new(file_rule_match::Column::RuleName)
+                            .string()
+                            .not_null(),
+                    )
                     .primary_key(
                         Index::create()
                             .name("unique_combo")
                             .table(file_rule_match::Entity.table_ref())
                             .col(file_rule_match::Column::FileId)
-                            .col(file_rule_match::Column::RuleName)
+                            .col(file_rule_match::Column::RuleName),
                     )
                     .foreign_key(
                         ForeignKey::create()
                             .name("file_name_id")
                             .to(files::Entity.table_ref(), files::Column::Id)
-                            .from(file_rule_match::Entity.table_ref(), file_rule_match::Column::FileId)
+                            .from(
+                                file_rule_match::Entity.table_ref(),
+                                file_rule_match::Column::FileId,
+                            )
                             .on_delete(ForeignKeyAction::Cascade)
-                            .on_update(ForeignKeyAction::Cascade)
+                            .on_update(ForeignKeyAction::Cascade),
                     )
-                    .to_owned()
-            ).await
+                    .to_owned(),
+            )
+            .await
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         // Replace the sample below with your own migration scripts
         manager
-            .drop_table(Table::drop().table(file_rule_match::Entity.table_ref()).to_owned())
+            .drop_table(
+                Table::drop()
+                    .table(file_rule_match::Entity.table_ref())
+                    .to_owned(),
+            )
             .await?;
 
         manager
